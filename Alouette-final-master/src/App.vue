592c452dcde7741a32ea0de9e9a29f58bf40d2ec
wilsontruong <90410232+wilsontruong@users.noreply.github.com>
2023-01-28 13:21:25 -0800

    Add files via upload

---
+++ Alouette-final-master/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <GameWindow/>
+  </div>
+</template>
+
+<script>
+
+import GameWindow from './components/GameWindow.vue';
+
+export default {
+  name: 'App',
+  components: {GameWindow
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-size:cover;
+  font-family: 'Roboto', sans-serif;
+  background-image: url('./assets/background.jpg');
+
+}
+</style>
